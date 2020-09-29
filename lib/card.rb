f4d7e1572ee38a611b8eb6b5e2c2f8e8c2b13ce2
Anna Kim <anna.kim93@gmail.com>
2020-09-29 00:34:11 -0400

    revised Card initializer to pass tests (raise arg errors)

--- lib/card.rb
+++ lib/card.rb
@@ -1,14 +1,21 @@
 
 # card.rb
+VALID_SUITS = [:hearts, :spades, :clubs, :diamonds]
 
 class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+    unless VALID_SUITS.include?(suit)
+      raise ArgumentError, "#{suit} is not a valid suit."
+    end
+
+    unless value.between?(1, 13) && value.class == Integer
+      raise ArgumentError, "#{value} is not a valid value."
+    end
+
     @value = value
     @suit = suit
-    
-
   end
 
   def to_s
